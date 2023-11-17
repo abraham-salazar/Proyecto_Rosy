e8aefdaee226c4fa4937ea847a3c25bf42d867ad
abraham-salazar <140665166+abraham-salazar@users.noreply.github.com>
2023-11-16 18:47:29 -0600

    primer actualizacion proyecto.c
    Lo que hice fue crear una funcion llamada menu para poder volver facilmente al menu principal con solo llamar a la funcion

--- proyecto.c
+++ proyecto.c
@@ -1,16 +1,13 @@
 #include <stdio.h>
-#include <windows.h>
+//#include <windows.h>
 #include <string.h>
 #include <locale.h>
-
-int main(){
-    setlocale(LC_ALL,"");
-	int opc = 0;
-	while (opc != 7){
+void menu(int opc){
+    	while (opc != 7){
 	printf("----------------------------------------\n");
 	printf("\tBIENVENIDO AL MENÚ\n");
 	printf("1.- Mostrar todos los contactos\n");
-	printf("2.- Buscar un registro");//Crear sub menú, 1 para que el parametro sea el nombre y el otro para que sea la clave
+	printf("2.- Buscar un registro\n");//Crear sub menú, 1 para que el parametro sea el nombre y el otro para que sea la clave
 	printf("3.- Ordenar alfabeticamente por clave, nombre o salario\n");
 	printf("4.- Insertar nuevos registros\n");
 	printf("5.- Eliminar un registro\n");//Crear submenu por clave o nombre
@@ -19,11 +16,11 @@ int main(){
 	printf("----------------------------------------\n");
 	scanf("%d", &opc);
 		if (opc == 1){
-			printf("Mostrar todos los contactos");
+			printf("Mostrar todos los contactos\n");
 			break;
 		}
 		if (opc == 2){
-            system("cls");
+            //system("cls");
 		    int opc2 = 0;
 		    while (opc2 != 3){
 		    printf("Seleccione una opcion: \n");
@@ -31,16 +28,62 @@ int main(){
 		    printf("2.- Buscar un registro por clave\n");
 		    printf("3.- Salir\n");
             scanf("%d", &opc2);
-
+        
 		    if(opc2 == 1){
-		        system("cls");
+                int opc3 = 0, cadena = 0;
+		        while(opc3 != 2){
+ 		        //system("cls");
+		        printf("\tBuscar un registro por nombre\n");
+		        printf("Ingrese el nombre que deseaa buscar: ");	
+		        scanf("%d", &cadena);
+		        //aqui para poner el scanf y hacer que busca la cadena de caracteres deseada
+		            printf("1.- Buscar otro: \n");
+		            printf("2.- Salir: \n");
+		            scanf("%d", &opc3);
+		            if(opc3 == 1){
+		                opc2 = 1;
+		                break;
+		            }
+		            if(opc3 == 2){
+                        menu(opc);
+		            }
+		            else if(opc3 != 1 & opc3 != 2){
+		                printf("Opcion incorrecta\nIntente nuevamente.\n");
+		        }
+		        
+		    }
+		    if(opc2 == 2){
+                //system("cls");
+                printf("\tBuscar un registro por clave\n");
+                printf("Ingrese la clave que desea buscar: \n");
+                //aqui para porner el scanf y hacer que busque la cadena deseada
+                break;
 		    }
 		    }
+		}
+		if(opc == 3){
+		    //system("cls");
+		    int opc2 = 0;
+		    printf("Ordenar alfabeticamente\n");
+		    while(opc2 != 4){
+		        printf("1.- Ordenar por nombre\n");
+		        printf("2.- Ordenar por clave\n");
+		        printf("3.- Ordenar por salario\n");
+		        scanf("%d", &opc2);
+		    }
+		}
+		else if(opc != 7 & opc != 1 & opc != 2 & opc != 3 & opc != 4 & opc != 5 & opc != 6){
+		    printf("Opcion incorrecta\nIntente nuevamente.\n");
 		}
 		}
 	
 	
 	
-	
+	}
+}
+int main(){
+    setlocale(LC_ALL,"");
+	int opc = 0;
+    menu(opc);
 	return 0;
 }
